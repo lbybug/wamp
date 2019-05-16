419e1e62fe32f0f4bf8df01f3b2bc6b514d08ff8
lbybug <825132005@qq.com>
2019-05-16 17:53:05 +0800

    add register

--- school/user/login.php
+++ school/user/login.php
@@ -11,56 +11,54 @@
 		die("连接失败: " . $con->connect_error);
 	}
 	
-	// $login_account = $_POST['account'];
+	$login_account = $_POST['account'];
 	
-	// $login_pass = $_POST['pass'];
+	$login_pass = $_POST['pass'];
 	
-	$login_account = $_GET['account'];
+	// $login_account = $_GET['account'];
 	
-	$login_pass = $_GET['pass'];
+	// $login_pass = $_GET['pass'];
+		
+	$sql = "select * from user where user_account = " .$login_account; //查询是否有这个账号
 	
-	echo md5($login_account);
+	$result = $con->query($sql);
 	
-	// $sql = "select * from user where user_account = " .$login_account; //查询是否有这个账号
+	$user = new UerBean();
 	
-	// $result = $con->query($sql);
+	$user->account = $login_account;
 	
-	// $user = new UerBean();
+	if($result->num_rows > 0){
+		$row = $result->fetch_assoc();
+		if($row['user_password'] == $login_pass){
+			$user->name = $row['user_name'];
+			$user->account = $row['user_account'];
+			$user->email = $row['user_email'];
+			$user->qq = $row['user_qq'];
+			$user->token = $row['user_token'];
+			$user->address = $row['user_address'];
+			$user->status = '0';
+			$user->error = '登陆成功';
+		}else{
+			$user->status = '1';
+			$user->error = '密码错误';
+		}
+	}else{
+		$user->status = '2';
+		$user->error = '用户还未注册';
+	}
 	
-	// $user->account = $login_account;
+	echo json_encode($user);
 	
-	// if($result->num_rows > 0){
-		// $row = $result->fetch_assoc();
-		// if($row['user_password'] == $login_pass){
-			// $user->name = $row['user_name'];
-			// $user->account = $row['user_account'];
-			// $user->email = $row['user_email'];
-			// $user->qq = $row['user_qq'];
-			// $user->token = $row['user_token'];
-			// $user->address = $row['user_address'];
-			// $user->status = '0';
-			// $user->error = '登陆成功';
-		// }else{
-			// $user->status = '1';
-			// $user->error = '密码错误';
-		// }
-	// }else{
-		// $user->status = '2';
-		// $user->error = '用户还未注册';
-	// }
+	$con->close();
 	
-	// echo json_encode($user);
-	
-	// $con->close();
-	
-	// class UerBean{
-		// public $name = '';
-		// public $account = '';
-		// public $email = '';
-		// public $qq = '';
-		// public $token = '';
-		// public $address = '';
-		// public $status = '';
-		// public $error = '';
-	// }
+	class UerBean{
+		public $name = '';
+		public $account = '';
+		public $email = '';
+		public $qq = '';
+		public $token = '';
+		public $address = '';
+		public $status = '';
+		public $error = '';
+	}
 ?>
---
+++ school/user/register.php
@@ -0,0 +1,78 @@
+﻿<?php
+
+	$dbHost = "127.0.0.1";
+	$dbUser = "root";
+	$dbPass = "061300";
+	$dbName = "school";
+	
+	$con = new mysqli($dbHost,$dbUser,$dbPass,$dbName);
+	
+	if($con->connect_error){
+		die("连接失败: " . $con->connect_error);
+	}
+	
+	$register_account = $_POST['account'];
+	
+	$register_pass = $_POST['pass'];
+	
+	$register_email = $_POST['email'];
+	
+	$register_qq = $_POST['qq'];
+	
+	$register_address = $_POST['address'];
+		
+	// $register_account = $_GET['account'];
+	
+	// $register_pass = $_GET['pass'];
+	
+	// $register_email = $_GET['email'];
+	
+	// $register_qq = $_GET['qq'];
+	
+	// $register_address = $_GET['address'];
+		
+	$sql = "select * from user where user_account = " .$register_account; //查询是否有这个账号
+	
+	$result = $con->query($sql);
+	
+	$user = new UerBean();
+		
+	if($result->num_rows > 0){
+		$user->status = '1';
+		$user->error ='用户已注册';
+	}else{
+		if($register_account != null
+		&& $register_pass != null
+		&& $register_email != null
+		&& $register_qq != null
+		&& $register_address != null){
+			$md5Pass = md5($register_pass);
+			$md5Token = md5($register_account);
+			
+			$insert_sql = "INSERT INTO user ".
+			"(user_account,user_password, user_name,user_email,user_qq,user_address,user_token,user_status) ".
+			"VALUES ".
+			"('$register_account','$md5Pass','$register_account','$register_email','$register_qq','$register_address','$md5Token','0')";
+				
+			if($con->query($insert_sql) == true){
+				$user->status = '0';
+				$user->error ='注册成功';
+			}else{
+				$user->status = '2';
+				$user->error ='注册失败';
+			}
+		}else{
+			$user->status = '2';
+			$user->error ='注册失败';
+		}
+	}
+	
+	echo json_encode($user);
+	
+	$con->close();
+	
+	class UerBean{
+		public $status = '';
+		public $error = '';
+	}
+?>
